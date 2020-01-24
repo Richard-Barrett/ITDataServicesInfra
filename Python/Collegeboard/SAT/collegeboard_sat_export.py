e767c4d21f1d7e4c4dc8dcbe11790c7ce276cc34
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-01-24 13:23:07 -0600

    Create collegeboard_sat_export.py

---
+++ Python/Collegeboard/SAT/collegeboard_sat_export.py
@@ -0,0 +1,27 @@
+#!/bin/python
+# ===========================================================
+# Created By: Richard Barrett
+# Organization: DVISD
+# DepartmenT: Data Services
+# Purpose: Test Score & 3rd Party Website Data Pull Automation
+# Date: 01/24/2020
+# ===========================================================
+
+import selenium
+import os
+import unittest
+import requests
+import time 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait 
+
+#Definitions
+#find_elements_by_name
+#find_elements_by_xpath
+#find_elements_by_link_text
+#find_elements_by_partial_link_text
+#find_elements_by_tag_name
+#find_elements_by_class_name
+#find_elements_by_css_selector
