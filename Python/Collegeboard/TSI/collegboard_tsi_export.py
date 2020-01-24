551924e19d267d707a225429592ae2f6df625eb9
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-01-23 18:09:20 -0600

    Update collegboard_tsi_export.py

--- Python/Collegeboard/TSI/collegboard_tsi_export.py
+++ Python/Collegeboard/TSI/collegboard_tsi_export.py
@@ -52,6 +52,14 @@ browser.find_element_by_css_selector('.btn.btn-lg.btn-primary').click()
 #browser.find_element_by_xpath('//*[@id="leftNav"]/ul/li[11]').click()
 browser.find_element_by_partial_link_text("Custom Reports").click()
 
+login_element = WebDriverWait(browser, 20).until(
+EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.pull-left")))
+login_element.click();
+
+custom_report_element = WebDriverWait(browser, 20).until(
+EC.element_to_be_clickable(By.XPATH, "//a[contains(@href, '/customReports')]"))
+custom_report_element.click();
+
 
 #element.send_keys(password)
 #element.send_keys(Keys.RETURN)
