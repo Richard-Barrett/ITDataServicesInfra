0b2527d64a8245e0cf52cc6cf4014d770fcf6d50
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-01-25 22:32:07 -0600

    Update act_scores_export.py

--- Python/ACT/act_scores_export.py
+++ Python/ACT/act_scores_export.py
@@ -17,11 +17,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait 
 
-#Definitions
-#find_elements_by_name
-#find_elements_by_xpath
-#find_elements_by_link_text
-#find_elements_by_partial_link_text
-#find_elements_by_tag_name
-#find_elements_by_class_name
-#find_elements_by_css_selector
+# Definitions
+# find_elements_by_name
+# find_elements_by_xpath
+# find_elements_by_link_text
+# find_elements_by_partial_link_text
+# find_elements_by_tag_name
+# find_elements_by_class_name
+# find_elements_by_css_selector
+
+# Define Driver Path
+
+# Define Parent URL
+
+# Define Login Process
+
+# Select ACT Test Scores and Reports
+
