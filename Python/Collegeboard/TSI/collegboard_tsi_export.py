d8640662a0a7efb1c0f07659679bcaeff748916b
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-01-27 12:27:20 -0600

    Update collegboard_tsi_export.py
    Added in the EC to fix an issue with primary code for expected_condition

--- Python/Collegeboard/TSI/collegboard_tsi_export.py
+++ Python/Collegeboard/TSI/collegboard_tsi_export.py
@@ -14,7 +14,7 @@ import requests
 import time 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait 
 
 # Definitions
