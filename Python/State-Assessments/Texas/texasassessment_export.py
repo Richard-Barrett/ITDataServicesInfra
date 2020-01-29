9c413c1e43f9ef40c1b9dceeddb6a6100bffb1fa
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-01-29 15:35:06 -0600

    Update texasassessment_export.py

--- Python/State-Assessments/Texas/texasassessment_export.py
+++ Python/State-Assessments/Texas/texasassessment_export.py
@@ -1,1 +1,38 @@
+#!/bin/python
+# ===========================================================
+# Created By: Richard Barrett
+# Organization: DVISD
+# DepartmenT: Data Services
+# Purpose: Test Score & 3rd Party Website Data Pull Automation
+# Date: 01/20/2020
+# ===========================================================
 
+import selenium
+import shutil
+import xlsxwriter
+import os
+import unittest
+import requests
+import time 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait 
+from datetime import date
+
+import json
+with open('secrets.json','r') as f:
+      config = json.load(f)
+
+# Definitions
+# find_elements_by_name
+# find_elements_by_xpath
+# find_elements_by_link_text
+# find_elements_by_partial_link_text
+# find_elements_by_tag_name
+# find_elements_by_class_name
+# find_elements_by_css_selector
+
+# System Variables
+today = date.today()
+date = today.strftime("%m/%d/%Y")
