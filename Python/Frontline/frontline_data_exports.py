58ad33a91a9502c0d9ad158d18706dadd8da61a6
Barrett <richard.barrett@dvisd.net>
2020-01-30 11:45:19 -0600

    Adding changes made to Frontline Files

--- Python/Frontline/frontline_data_exports.py
+++ Python/Frontline/frontline_data_exports.py
@@ -1,2 +1,56 @@
 #!/bin/python
+# ===========================================================
+# Created By: Richard Barrett
+# Organization: DVISD
+# DepartmenT: Data Services
+# Purpose: Test Score & 3rd Party Website Data Pull Automation
+# Date: 01/20/2020
+# ===========================================================
 
+import selenium
+import shutil
+import xlsxwriter
+import os
+import unittest
+import requests
+import getpass
+import time 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait 
+from datetime import date
+
+import json
+with open('secrets.json','r') as f:
+          config = json.load(f)
+
+          # Definitions
+          # find_elements_by_name
+          # find_elements_by_xpath
+          # find_elements_by_link_text
+          # find_elements_by_partial_link_text
+          # find_elements_by_tag_name
+          # find_elements_by_class_name
+          # find_elements_by_css_selector
+
+          # System Variables
+          today = date.today()
+          date = today.strftime("%m/%d/%Y")
+          username = getpass.getuser()
+
+          # URL Variables 
+          login_url = ''
+          redirect_url = ''
+
+          # WebDriver Path for Windows 10 
+          browser = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\chromedriver.exe")
+
+          # Parent URL
+          browser.get("https://login.frontlineeducation.com/login?signin=d5549b9f95fab5102b235b61100cd585&productId=estarV3&clientId=estarV3#/login")
+
+          # Credentials NEEDS TO BE ENCRYPTED AND NOT BAKED INTO THE SCRIPT NEEDS UNIT TEST
+          username = browser.find_element_by_id("login")
+          password = browser.find_element_by_id("password")
+          username.send_keys(config['user']['name'])
+          password.send_keys(config['user']['password'])
