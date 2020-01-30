346868340ef79b8b7d3f606811a4c621db618133
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-01-30 14:14:37 -0600

    Update skyward_employee_directory_export.py

--- Python/Skyward/skyward_employee_directory_export.py
+++ Python/Skyward/skyward_employee_directory_export.py
@@ -1,4 +1,3 @@
-
 #!/bin/python
 # ===========================================================
 # Created By: Richard Barrett
@@ -9,9 +8,12 @@
 # ===========================================================
 
 import selenium
+import shutil
+import xlsxwriter
 import os
 import unittest
 import requests
+import getpass
 import time 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -21,17 +23,40 @@ from datetime import date
 
 import json
 with open('secrets.json','r') as f:
-      config = json.load(f)
+          config = json.load(f)
 
-# Definitions
-# find_elements_by_name
-# find_elements_by_xpath
-# find_elements_by_link_text
-# find_elements_by_partial_link_text
-# find_elements_by_tag_name
-# find_elements_by_class_name
-# find_elements_by_css_selector
+          # Definitions
+          # find_elements_by_name
+          # find_elements_by_xpath
+          # find_elements_by_link_text
+          # find_elements_by_partial_link_text
+          # find_elements_by_tag_name
+          # find_elements_by_class_name
+          # find_elements_by_css_selector
 
-# System Variables
-today = date.today()
-date = today.strftime("%m/%d/%Y")
+          # System Variables
+          today = date.today()
+          date = today.strftime("%m/%d/%Y")
+          username = getpass.getuser()
+
+          # URL Variables 
+          login_url = ''
+          redirect_url = ''
+
+          # WebDriver Path for Windows 10 
+          browser = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\chromedriver.exe")
+
+          # Parent URL
+          browser.get("")
+
+          # Credentials NEEDS TO BE ENCRYPTED AND NOT BAKED INTO THE SCRIPT NEEDS UNIT TEST
+          username = browser.find_element_by_id("Username")
+          password = browser.find_element_by_id("Password")
+          username.send_keys(config['user']['name'])
+          password.send_keys(config['user']['password'])
+
+          # Authentication submit.click()
+          # For XPATH = //*[@id='qa-button-login']
+          element = WebDriverWait(browser, 20).until(
+                          EC.element_to_be_clickable((By.XPATH, "//*[@id='qa-button-login']")))
+          element.click();
