36bcd1c3617cf56d3049ef2a91412bdf4f205686
Barrett <richard.barrett@dvisd.net>
2020-02-06 09:47:21 -0600

    Adding in  Encryption for Frontline Modules

---
+++ Python/Frontline/frontline_stateassessments_missing_report.py
@@ -0,0 +1,36 @@
+#!/bin/python
+# ===========================================================
+# Created By: Richard Barrett
+# Organization: DVISD
+# DepartmenT: Data Services
+# Purpose: Test Score & 3rd Party Website Data Pull Automation
+# Date: 01/20/2020
+# ===========================================================
+
+import selenium
+import shutil
+import xlsxwriter
+import os
+import unittest
+import requests
+import subprocess
+import getpass
+import time 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait 
+from datetime import date
+import json
+import logging 
+
+decrypt = "gpg --output secrets.json --decrypt secrets.gpg" 
+
+if os.path.exists("secrets.gpg"):
+      returned_value = subprocess.call(decrypt, shell=True)
+else:
+        print("The file does not exist")
+
+with open('secrets.json','r') as f:
+          config = json.load(f)
+
