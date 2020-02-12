6932df4a0a86e9c980e8ff7c802e718e515fd6f8
Barrett <richard.barrett@dvisd.net>
2020-02-12 16:40:44 -0600

    Adding in an Internal Metric for File Comparison

--- Python/Analyses/Internal/nutrikids_skyward_comparison.py
+++ Python/Analyses/Internal/nutrikids_skyward_comparison.py
@@ -1,1 +1,68 @@
+#!/bin/python
+# ===========================================================
+# Created By: Richard Barrett
+# Organization: DVISD
+# DepartmenT: Data Services
+# Purpose: Test Score & 3rd Party Website Data Pull Automation
+# Date: 01/20/2020
+# ===========================================================
 
+import selenium
+import shutil
+import xlsxwriter
+import os
+import unittest
+import requests
+import subprocess
+import getpass
+import platform
+import logging
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from datetime import date
+
+decrypt = "gpg --output secrets.json --decrypt secrets.gpg"
+
+if os.path.exists("secrets.gpg"):
+    returned_value = subprocess.call(decrypt, shell=True)
+else:
+    print("The file does not exist")
+
+import json
+with open('secrets.json','r') as f:
+    config = json.load(f)
+
+    # Definitions
+    # find_elements_by_name
+    # find_elements_by_xpath
+    # find_elements_by_link_text
+    # find_elements_by_partial_link_text
+    # find_elements_by_tag_name
+    # find_elements_by_class_name
+    # find_elements_by_css_selector
+
+# System Variables
+today = date.today()
+date = today.strftime("%m/%d/%Y")
+node = platform.node()
+system = platform.system()
+username = getpass.getuser()
+version = platform.version()
+
+# Check for Version of Chrome
+
+# WebDriver Path for System
+if platform.system() == ('Windows'):
+    browser = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\chromedriver.exe")
+elif platform.system() == ('Linux'):
+    browser = webdriver.Chrome(executable_path='/home/rbarrett/Drivers/Google/Chrome/chromedriver_linux64/chromedriver')
+elif platform.system() == ('Darwin'):
+    browser = webdriver(executable_path='~/Drivers/Google/Chrome/chromedriver_mac64/chromedriver')
+else:
+    print("Are you sure you have the Selenium Webdriver installed in the correct path?")
+                          
+# Parent URL
+browser.get("https://eduphoria.del-valle.k12.tx.us/AuthDistrict/Login?ReturnUrl=/default.aspx")
