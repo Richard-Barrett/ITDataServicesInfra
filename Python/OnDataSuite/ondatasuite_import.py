f89bc69e8a62c62fbf96f6c936511d5840d60410
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-02-13 08:28:53 -0600

    Update ondatasuite_import.py

--- Python/OnDataSuite/ondatasuite_import.py
+++ Python/OnDataSuite/ondatasuite_import.py
@@ -1,1 +1,8 @@
-
+#!/bin/python
+# ===========================================================
+# Created By: Richard Barrett
+# Organization: DVISD
+# DepartmenT: Data Services
+# Purpose: Test Score & 3rd Party Website Data Pull Automation
+# Date: 01/20/2020
+# ===========================================================
