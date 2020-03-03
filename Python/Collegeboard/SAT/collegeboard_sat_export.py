0fd83f00ac71aec2ccd1bf5df00cae7e47f7ef7c
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-03 13:41:33 -0600

    Update collegeboard_sat_export.py

--- Python/Collegeboard/SAT/collegeboard_sat_export.py
+++ Python/Collegeboard/SAT/collegeboard_sat_export.py
@@ -13,9 +13,13 @@ import xlsxwriter
 import os
 import unittest
 import requests
+import subprocess
 import getpass
+import platform
+import logging
 import time 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait 
