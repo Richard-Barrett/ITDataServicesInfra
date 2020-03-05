1dfa46f31fa94af93b9805559029245b9e4e60b8
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-05 09:21:40 -0600

    Update git_pull.ps1

--- Administration/Git/git_pull.ps1
+++ Administration/Git/git_pull.ps1
@@ -1,3 +1,14 @@
 #!/bin/powershell
-cd C:/Git/ITDataServicesInfra/
-git pull
+
+# Set-Working Directory 
+Set-Location ~
+$GIT-PATH = Test-Path ~\Git\ITDataServicesInfra\
+
+If ($GIT-PATH == True) {
+  'Set-Location ~\Git\ITDataServicesInfra\ -and git pull'
+
+  }  Else {
+
+  'Are you sure that the repository is in the correct location?'
+
+} 
