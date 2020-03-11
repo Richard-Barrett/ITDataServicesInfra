a5867651954f2b24ca7a68ee2cd8955a3c37d750
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-11 11:56:25 -0500

    Update frontline_sftp_put.py

--- SFTP/Frontline/frontline_sftp_put.py
+++ SFTP/Frontline/frontline_sftp_put.py
@@ -1,31 +1,60 @@
-import paramiko
 import os
-
-# os.remove('peimsexp.csv')
-
+import paramiko
+import sys
+import json
+with open('secrets.json','r') as f:
+      config = json.load(f)
+    
 # set up logging
 from paramiko.py3compat import input
 
-paramiko.util.log_to_file('peims_ftp_log')
+paramiko.util.log_to_file('clever_ftp_log')
 
 make_ssh_connexion = paramiko.SSHClient()
 make_ssh_connexion.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-host = "<host>"
-user_name = "<username>"
-pass_word = "<passowrd>"
-port_num = "<port>"
-# print("Connecting...")
-# print(host)
-# print(key_file)
-make_ssh_connexion.connect(hostname = host, username = user_name, password = pass_word)
+# JSON File Should Contain the Folling Information
+# File should be named as secrets.json
+# Category should be sftp_credential
+
+# Nested Objects
+# host= '<hostname>'
+# username="<username>"
+# password="<password>"
+# port_num="<port>"
+# key_file=c
+
+print("Connecting...")
+print((config['sftp_crentential']['host']))
+print((config['sftp_crentential']['key_file']))
+
+make_ssh_connexion.connect(hostname = (config['sftp_crentential']['host']), 
+                           username = (config['sftp_crentential']['username']), 
+                           password = (config['sftp_crentential']['password']))
 
 # print("connected!")
 sftp = make_ssh_connexion.open_sftp()
 
-if os.path.exists('peimsexp.csv'):
-    os.remove('peimsexp.csv')
-sftp.get('peimsexp.csv', 'peimsexp.csv')
-sftp.put('peimsexp.csv', 'peimsexp.csv')
+with open('files.json','r') as f:
+      file = json.load(f)
+ 
+# JSON File Should Contain the Folling Information
+# File Should be named files.json
+# File should be referenced as Files:
+# After Files: File_1 should be nested as "file_1":"/Directory/Path/Filename"
+# You can Change the files.json File by enumerating with file_# as an index.
+
+
+## Examples
+# if os.path.exists('peimsexp.csv'):
+#     os.remove('peimsexp.csv')
+# sftp.get('peimsexp.csv', 'peimsexp.csv')
+# sftp.put('peimsexp.csv', 'peimsexp.csv')
+
+sftp.put((file['files']['file_1']), 
+         (file['files']['file_2']),
+         (file['files']['file_3']),
+        )
 
 make_ssh_connexion.close()
+print("Process Complete!")
