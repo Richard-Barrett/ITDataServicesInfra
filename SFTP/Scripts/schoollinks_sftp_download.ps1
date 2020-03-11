3889d3bfb2143bde17c751e5bad9986951fd244a
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-11 09:24:16 -0500

    Create schoollinks_sftp_download.ps1

---
+++ SFTP/Scripts/schoollinks_sftp_download.ps1
@@ -0,0 +1,56 @@
+param (
+    $localPath = 'E:\FTP\SchooLinks\course_requests.csv',
+    $remotePath = '/schoolinks_exports/course_planning/course_requests.csv'
+)
+ 
+try
+{
+    # Load WinSCP .NET assembly
+    Add-Type -Path "C:\Program Files (x86)\WinSCP\WinSCPnet.dll"
+ 
+    # Setup session options
+    $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+        Protocol = [WinSCP.Protocol]::Sftp
+        HostName = "<domain_name>"
+        UserName = "<username>"
+        Password = "<password>"
+        SshHostKeyFingerprint = "<fingerprint>"
+    }
+ 
+    $session = New-Object WinSCP.Session
+ 
+    try
+    {
+        # Connect
+        $session.Open($sessionOptions)
+ 
+        # Upload files
+        $transferOptions = New-Object WinSCP.TransferOptions
+        $transferOptions.TransferMode = [WinSCP.TransferMode]::Binary
+ 
+        $transferResult =
+            $session.GetFiles($remotePath, $localPath, $False, $transferOptions)
+ 
+        # Throw on any error
+        $transferResult.Check()
+ 
+        # Print results
+        foreach ($transfer in $transferResult.Transfers)
+        {
+            Write-Host "Download of $($transfer.FileName) succeeded"
+        }
+    }
+    finally
+    {
+        # Disconnect, clean up
+        $session.Dispose()
+    }
+ 
+    exit 0
+}
+catch
+{
+    Write-Host "Error: $($_.Exception.Message)"
+    exit 1
+}
+
