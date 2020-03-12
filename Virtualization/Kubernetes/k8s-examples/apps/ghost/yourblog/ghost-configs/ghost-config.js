5dde4ff673ba37dfdbde52952f890d8405ebb477
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-12 13:59:03 -0500

    Create ghost-config.js

---
+++ Virtualization/Kubernetes/k8s-examples/apps/ghost/yourblog/ghost-configs/ghost-config.js
@@ -0,0 +1,56 @@
+var path = require('path'),
+    config;
+
+config = {
+    // ### Production
+    production: {
+        url: 'http://my-ghost-blog.com',
+        mail: {},
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(process.env.GHOST_CONTENT, '/data/ghost.db')
+            },
+            debug: false
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2368'
+        }
+    },
+
+    // ### Development **(default)**
+    development: {
+        // The url to use when providing links to the site, E.g. in RSS and email.
+        // Change this to your Ghost blog's published URL.
+        url: 'http://13.57.234.247:32556',
+
+        // #### Database
+        database: {
+            client: 'mysql',
+            connection: {
+              host    : 'mysql-internal.myblog',
+              user    : process.env.GHOST_DB_USER,
+              password  : process.env.GHOST_DB_PASSWORD,
+              database  : process.env.GHOST_DB_NAME,
+              charset : 'utf8'
+            },
+            debug: true
+        },
+
+        // #### Server
+        server: {
+            host: '0.0.0.0',
+            port: '2368'
+        },
+
+        // #### Paths
+        // Specify where your content directory lives
+        paths: {
+            contentPath: path.join(process.env.GHOST_CONTENT, '/')
+        }
+    },
+
+};
+
+module.exports = config;
