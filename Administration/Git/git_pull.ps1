d45e9b7cbf3e6b9b95720f7fab4aa849ce20b77e
Barrett <richard.barrett@dvisd.net>
2020-03-13 10:02:40 -0500

    Commenting out Non-Working Code using git pull

--- Administration/Git/git_pull.ps1
+++ Administration/Git/git_pull.ps1
@@ -1,14 +1,15 @@
 #!/bin/powershell
+git pull
 
 # Set-Working Directory 
-Set-Location ~
-$GIT-PATH = Test-Path ~\Git\ITDataServicesInfra\
+#Set-Location ~
+#$GIT-PATH = Test-Path ~\Git\ITDataServicesInfra\
 
-If ($GIT-PATH == True) {
-  'Set-Location ~\Git\ITDataServicesInfra\ -and git pull'
-
-  }  Else {
-
-  'Are you sure that the repository is in the correct location?'
-
-} 
+#If ($GIT-PATH == True) {
+#  'Set-Location ~\Git\ITDataServicesInfra\ -and git pull'
+#
+#  }  Else {
+#
+#  'Are you sure that the repository is in the correct location?'
+#
+#} 
