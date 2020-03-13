dac2732903589c3bc258abe58b29265484b09c0f
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-12 21:20:16 -0500

    Update docker_install_linux.sh

--- Virtualization/Docker/Installation/docker_install_linux.sh
+++ Virtualization/Docker/Installation/docker_install_linux.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+sudo apt-get update
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" 
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
