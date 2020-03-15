271953179e189d64886084569f5afb6ae5749fbd
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-15 12:58:35 -0500

    Create sftp_secrets_maker.py

---
+++ SFTP/Secrets/sftp_secrets_maker.py
@@ -0,0 +1,23 @@
+#!/bin/python
+
+import json
+
+# example dictionary that contains data like you want to have in json
+details = {
+  "sftp_credential": {
+    "host": "<hostname>",
+    "username": "<username>",
+    "password": "password",
+    "port_num": "<port>",
+    "key_file": "<direct_path>"
+  }
+}
+
+with open('secrets.json', 'w') as json_file:
+    json.dump(details, json_file, indent=4)
+
+# get json string from that dictionary
+json = json.dumps(details, indent=4)
+print("JSON will be STD.OUT to secrets.json")
+print("Please check the current working directory for the file.")
+print json
