cb76def11a981d3b69fbde3cf4da9cac3ff7e8a9
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-28 17:37:30 -0500

    Create secrets_json_skyward_maker.py

---
+++ SFTP/Skyward/secrets_json_skyward_maker.py
@@ -0,0 +1,24 @@
+#!/bin/user python
+
+import json
+
+# example dictionary that contains data like you want to have in json
+# "slack_target_url": "https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXXXXX/XXXXXXXXXXXXXXX"
+details = {
+  "sftp_credential": {
+    "host": "<hostname>",
+    "username": "<username>",
+    "password": "password",
+    "port_num": "<port>",
+    "key_file": "<direct_path>"
+  }
+}
+
+with open('secrets.json', 'w') as json_file:
+    json.dump(details, json_file, indent=4)
+
+# get json string from that dictionary
+json = json.dumps(details, indent=4)
+print("JSON will be STD.OUT to secrets.json")
+print("Please check the current working directory for the file.")
+print(json)
