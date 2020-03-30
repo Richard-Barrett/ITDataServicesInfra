dbf375049eaf952415eaf2aaf12a3107a9e67789
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-30 09:58:45 -0500

    Update Choco_Developer_Pacakages.ps1

--- Administration/Windows/Chocolatey/Packages/Choco_Developer_Pacakages.ps1
+++ Administration/Windows/Chocolatey/Packages/Choco_Developer_Pacakages.ps1
@@ -1,2 +1,5 @@
+#!/bin/powershell
+
+# Installs All Packages 
 # Packages that need to be installed for developer work on Local Machine 
-choco install googlechrome
+cat .\Developer_Packages.txt | ForEach-Object { choco install $_ -y}
