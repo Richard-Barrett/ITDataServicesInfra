ad8fdaf2867e85bf1dbcbbf9cf0257434beb6aec
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-30 10:05:47 -0500

    Delete Choco_Developer_Pacakages.ps1

--- Administration/Windows/Chocolatey/Packages/Choco_Developer_Pacakages.ps1
+++
@@ -1,5 +0,0 @@
-#!/bin/powershell
-
-# Installs All Packages 
-# Packages that need to be installed for developer work on Local Machine 
-cat .\Developer_Packages.txt | ForEach-Object { choco install $_ -y}
