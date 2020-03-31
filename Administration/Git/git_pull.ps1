0dc1a573dac307e0d5792bf6d5e1b65a111a6f65
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-30 20:02:19 -0500

    Update git_pull.ps1

--- Administration/Git/git_pull.ps1
+++ Administration/Git/git_pull.ps1
@@ -1,15 +1,4 @@
 #!/bin/powershell
-git pull
 
-# Set-Working Directory 
-#Set-Location ~
-#$GIT-PATH = Test-Path ~\Git\ITDataServicesInfra\
-
-#If ($GIT-PATH == True) {
-#  'Set-Location ~\Git\ITDataServicesInfra\ -and git pull'
-#
-#  }  Else {
-#
-#  'Are you sure that the repository is in the correct location?'
-#
-#} 
+# Initialize git pull recursively
+Get-ChildItem -Path C:\Users\richard.barrett\Git\ -Name | Select-String -Pattern ".secrets" -NotMatch | ForEach-Object {cd $_; git pull; cd ..}
