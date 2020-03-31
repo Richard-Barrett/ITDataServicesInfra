db15326641665dd32305476aa36963c37e39c7f9
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-30 20:03:07 -0500

    Update git_pull.ps1

--- Administration/Git/git_pull.ps1
+++ Administration/Git/git_pull.ps1
@@ -1,4 +1,4 @@
 #!/bin/powershell
 
 # Initialize git pull recursively
-Get-ChildItem -Path C:\Users\richard.barrett\Git\ -Name | Select-String -Pattern ".secrets" -NotMatch | ForEach-Object {cd $_; git pull; cd ..}
+Get-ChildItem -Path ~\Git\ -Name | Select-String -Pattern ".secrets" -NotMatch | ForEach-Object {cd $_; git pull; cd ..}
