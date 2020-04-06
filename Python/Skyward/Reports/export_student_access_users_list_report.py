b99c44dc80ea66e3d9e3bbfadb7c85a57fe7192a
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-04-06 08:11:46 -0500

    Update export_student_access_users_list_report.py

--- Python/Skyward/Reports/export_student_access_users_list_report.py
+++ Python/Skyward/Reports/export_student_access_users_list_report.py
@@ -153,6 +153,14 @@ element.click();
 element = WebDriverWait(browser, 20).until(
     EC.element_to_be_clickable((By.XPATH, "//*[@id='bExport']")))
 element.click();
+time.sleep(15)
+
+# Click on Download Export Button
+# XPATH = //*[@id='printMsgBtn1']
+element = WebDriverWait(browser, 20).until(
+    EC.element_to_be_clickable((By.XPATH, "//*[@id='printMsgBtn1']")))
+element.click();
+time.sleep(2)
 
 # Click on id="browsetool_export"
 # Opens Options to Download Report
@@ -172,6 +180,7 @@ element.click();
 element = WebDriverWait(browser, 20).until(
     EC.element_to_be_clickable((By.XPATH, "//*[@id='bExport']")))
 element.click();
+time.sleep(20)
 
 # Click on Download Export Button
 # XPATH = //*[@id='printMsgBtn1']
@@ -192,4 +201,5 @@ else:
               
 # Close Browser Session Gracefully              
 print("The Export was successfull!")
+shutDown(self)
 #browser.quit()
